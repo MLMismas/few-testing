d1e148b89a8942b8f614bfb95d2b7b388ea1d53e
MLMismas <52412840+MLMismas@users.noreply.github.com>
2021-03-10 14:15:29 -0500

    for jamie

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,4 +1,5 @@
 import { ActionReducerMap, createSelector } from '@ngrx/store';
+import { SongListItem } from '../models/songs';
 import * as fromCounter from './counter.reducer';
 import * as fromSongs from './songs.reducer';
 export interface AppState {
@@ -12,9 +13,39 @@ export const reducers: ActionReducerMap<AppState> = {
 };
 
 export const _selectCounterBranch = (state: AppState) => state.counter;
+export const _selectSongBranch = (state: AppState) => state.songs;
 
 // for the components
 export const selectCurrentCounter = createSelector(
   _selectCounterBranch,
   b => b.current
+);
+
+export const _selectSongsSortBy = createSelector(
+  _selectSongBranch,
+  b => b.sortBy
+);
+
+export const { selectAll: _selectSongEntityArray } =
+  fromSongs.adapter.getSelectors(_selectSongBranch);
+
+export const _selectSongListItems = createSelector(
+  _selectSongEntityArray,
+  a => a as SongListItem[]
+);
+
+export const selectSortedSongListItems = createSelector(
+  _selectSongsSortBy,
+  _selectSongListItems,
+  (by, items) => {
+    return [...items.sort((lhs, rhs) => {
+      if (lhs[by].toLowerCase() < rhs[by].toLowerCase()) {
+        return -1;
+      }
+      if (lhs[by].toLowerCase() > rhs[by].toLowerCase()) {
+        return 1;
+      }
+      return 0;
+    })];
+  }
 );
